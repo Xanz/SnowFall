7c221a2dac72a161a7834502f3491c4e3ee03197
Xanz <xanrealmz@hotmail.co.uk>
2024-05-24 15:50:16 +0100

    submodule added

--- Deploy.bat
+++ Deploy.bat
@@ -8,9 +8,6 @@ echo "Deploy shaders .EXE locations"
 xcopy ".\shaders" "build/Debug/base/" /E /H /C /I
 xcopy ".\shaders" "build/Release/base/" /E /H /C /I
 
-echo "Deploy library dynamic libs"
-xcopy ".\external\glfw\libs\glfw3.dll" ".\build\Debug\" /H /C /Y
-xcopy ".\external\OpenAL\build\Debug\OpenAL32.dll" ".\build\Debug\" /H /C /Y
-
-xcopy ".\external\glfw\libs\glfw3.dll" ".\build\Release\" /H /C /Y
-xcopy ".\external\OpenAL\build\Debug\OpenAL32.dll" ".\build\Release\" /H /C /Y
+@REM echo "Deploy library dynamic libs"
+xcopy ".\build\external\openal\Debug\OpenAL32.dll" ".\build\Debug\" /H /C /Y
+xcopy ".\build\external\openal\Release\OpenAL32.dll" ".\build\Release\" /H /C /Y
