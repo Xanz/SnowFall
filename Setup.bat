834dff522c47c0d66fd2f4478826117e8219bec8
Xanz <xanrealmz@hotmail.co.uk>
2024-10-11 12:20:14 +0100

    Fixes

--- Setup.bat
+++ Setup.bat
@@ -3,17 +3,17 @@ echo "Submodule download"
 git submodule init
 git submodule update
 
-echo "GLFW build"
-pushd "external/glfw"
-cmake -DBUILD_SHARED_LIBS=OFF -B build -S .
-cmake --build ./build --config Release
-popd
-
-echo "OpenAL build"
-pushd "external/openal"
-cmake -B build -S .
-cmake --build ./build --config Release
-popd
-
-echo "Copy openal dll"
-xcopy .\external\openal\build\Release\OpenAL32.dll .\ /Y
+@REM echo "GLFW build"
+@REM pushd "external/glfw"
+@REM cmake -DBUILD_SHARED_LIBS=OFF -B build -S .
+@REM cmake --build ./build --config Release
+@REM popd
+@REM
+@REM echo "OpenAL build"
+@REM pushd "external/openal"
+@REM cmake -B build -S .
+@REM cmake --build ./build --config Release
+@REM popd
+@REM
+@REM echo "Copy openal dll"
+@REM xcopy .\external\openal\build\Release\OpenAL32.dll .\ /Y
